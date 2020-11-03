58270c73f1c18d084b7a61a033c24eee6ed04e06
DONROB3R7 <costache.robert.valentin@gmail.com>
2020-11-03 19:43:19 +0100

    Added the ProgressBar component Added the progress bar component Updated the firebase configuration Updated the styles CSS for the progress bar. Updated the functionality for Upload files

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -1,9 +1,9 @@
-import * as firebase from "firebase/app";
+import firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/firestore";
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
+var config = {
   apiKey: "AIzaSyBaBMdDyEotJCBTlCFg87ZdEMpv7sZWblM",
   authDomain: "fir-gallery-1b939.firebaseapp.com",
   databaseURL: "https://fir-gallery-1b939.firebaseio.com",
@@ -12,10 +12,14 @@ var firebaseConfig = {
   messagingSenderId: "117799571564",
   appId: "1:117799571564:web:bd33574ff2fec26341c849"
 };
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
 
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { projectFirestore, projectStorage };
+export { projectFirestore, projectStorage, timestamp };
