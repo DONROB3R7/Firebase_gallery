0035c9f2d3c25b14532f1e9c064a5253cb3a3067
DONROB3R7 <costache.robert.valentin@gmail.com>
2020-11-04 20:50:04 +0100

    Updated the Firebase confing Update the firebase config to render from DB the images. Implemented the Modal Component for each image. Updated the CSS for the grid model.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,35 @@ import "./styles.css";
 
 import Title from "./componets/Title";
 import UploadForm from "./componets/UploadForm";
+import ImageGrid from "./componets/ImageGrid";
+import Modal from "./componets/Modal";
 
 class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ModalImg: null
+    };
+  }
+
+  setStatedImg = (link) => {
+    this.setState({ ModalImg: link });
+    console.log(link);
+  };
+
   render() {
     return (
       // title and uploadForm component
       <div className="Gallery_App">
         <Title />
         <UploadForm />
+        <ImageGrid setSelectedImg={this.setStatedImg} />
+        {this.state.ModalImg && (
+          <Modal
+            selectedImg={this.state.ModalImg}
+            setSelectedImg={this.setStatedImg}
+          />
+        )}
       </div>
     );
   }
